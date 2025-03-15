58881fda03e2a27e9322dfa405337393963d17c5
Denys <djkjibyltybc@gmail.com>
2025-03-15 11:18:07 +0200

    dictionary implementation, correction

--- app/main.py
+++ app/main.py
@@ -2,15 +2,13 @@ class Dictionary:
     def __init__(self, initial_capacity: int = 8) -> None:
         self.capacity: int = initial_capacity
         self.size: int = 0
-        self.buckets: list[list[list]] = [None] * self.capacity
+        self.buckets: list[list[list]] = [[] for _ in range(self.capacity)]
 
     def _hash(self, key: object) -> int:
         return hash(key) % self.capacity
 
     def __setitem__(self, key: object, value: object) -> None:
         index: int = self._hash(key)
-        if self.buckets[index] is None:
-            self.buckets[index] = []
 
         for pair in self.buckets[index]:
             if pair[0] == key:
@@ -25,11 +23,12 @@ class Dictionary:
 
     def __getitem__(self, key: object) -> object:
         index: int = self._hash(key)
-        if self.buckets[index] is not None:
-            for pair in self.buckets[index]:
-                if pair[0] == key:
-                    return pair[1]
-        raise KeyError(f'Key {key} not found')
+
+        for pair in self.buckets[index]:
+            if pair[0] == key:
+                return pair[1]
+
+        raise KeyError(f'Ключ "{key}" не найден в словаре')
 
     def __len__(self) -> int:
         return self.size
@@ -37,10 +36,12 @@ class Dictionary:
     def _resize(self) -> None:
         old_buckets: list[list[list]] = self.buckets
         self.capacity *= 2
-        self.buckets = [None] * self.capacity
+        self.buckets = [[] for _ in range(self.capacity)]
         self.size = 0
 
         for bucket in old_buckets:
-            if bucket:
-                for key, value in bucket:
-                    self[key] = value
+            for key, value in bucket:
+                index = self._hash(key)
+                if not any(pair[0] == key for pair in self.buckets[index]):
+                    self.buckets[index].append([key, value])
+                    self.size += 1

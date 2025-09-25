67789be59bfbb83b5ed3bc349bb18252c45003c0
Radoslaw Ptak <radek1026@interi.pl>
2025-09-25 16:56:16 +0200

    Implement py-dictionary

--- app/main.py
+++ app/main.py
@@ -1,2 +1,44 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+
+
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.load_factor = 0.75
+        self.size = 0
+        self.buckets = [[] for _ in range(self.capacity)]
+
+    def _index_for(self, h: int) -> int:
+        return h % self.capacity
+
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __getitem__(self, key: int) -> Any:
+        h = hash(key)
+        i = self._index_for(h)
+        if i not in self.buckets[i]:
+
+
+    def __setitem__(self, key: int, value: Any) -> None:
+        h = hash(key)
+
+
+
+
+    def find_bucket_entry(self, h: int, key: int, value: int) -> Any:
+
+
+
+
+
+
+
+
+
+
+
+

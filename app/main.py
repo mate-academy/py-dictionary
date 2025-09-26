92a4df3869ad3197931a8f597537bd3b416d383b
Radoslaw Ptak <radek1026@interi.pl>
2025-09-26 12:34:38 +0200

    CR change in implement py-dictionary

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Tuple, Optional
+from typing import Any, Tuple, Optional
 
 
 class Node:
@@ -15,17 +15,17 @@ class Dictionary:
         self.capacity = 8
         self.load_factor = 0.75
         self.size = 0
-        self.buckets = List[List[Node]] = [[] for _ in range(self.capacity)]
+        self.buckets = [[] for _ in range(self.capacity)]
 
     def _index_for(self, h: int) -> int:
         return h % self.capacity
 
-    def _find_node(self, kay: Any, h: int) -> Tuple[Optional[Node], int]:
+    def _find_node(self, key: Any, h: int) -> Tuple[Optional[Node], int]:
         index = self._index_for(h)
         bucket = self.buckets[index]
 
         for i, node in enumerate(bucket):
-            if node.key == kay and node.hash == h:
+            if node.hash == h and node.key == key:
                 return node, i
         return None, -1
 
@@ -34,24 +34,24 @@ class Dictionary:
 
     def __getitem__(self, key: Any) -> Any:
         h = hash(key)
-        node, i = self._find_node(key, h)
+        node, _ = self._find_node(key, h)
 
         if node:
             return node.value
         else:
             raise KeyError(f"Key not found: {key}")
 
-    def __setitem__(self, key: int, value: Any) -> None:
+    def __setitem__(self, key: Any, value: Any) -> None:
         h = hash(key)
         index = self._index_for(h)
         bucket = self.buckets[index]
-        node, i = self._find_node(key, h)
+        node, _ = self._find_node(key, h)
 
         if node:
             node.value = value
             return
         new_node = Node(key, value, h)
-        bucket[index].append(new_node)
+        bucket.append(new_node)
         self.size += 1
 
         if self.size / self.capacity > self.load_factor:

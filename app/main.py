20b2e15f9bc5d6123fe5413796adcdf61216712b
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-24 20:31:31 +0200

    added sentinel value

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from typing import Iterator, Any
 
 class Dictionary:
     def __init__(self, **kwargs) -> None:
-        self.length = len(kwargs)
+        self.length = 0
         self.capacity = 8
         self.load_factor = 2 / 3
-        while self.length > self.capacity * self.load_factor:
+        self.DELETED = object()
+        while len(kwargs) > self.capacity * self.load_factor:
             self.capacity *= 2
         self.hash_table = [[] for _ in range(self.capacity)]
         _max_table_index = len(self.hash_table) - 1
@@ -35,39 +36,42 @@ class Dictionary:
                     break
                 ind = (ind + 1) % self.capacity
             else:
-                self.hash_table[ind] = [key, hash(key), value]
-                self.length += 1
-                break
+                if (self.hash_table[ind] == []
+                        or self.hash_table[ind] is self.DELETED):
+                    self.hash_table[ind] = [key, hash(key), value]
+                    self.length += 1
+                    break
 
         if self.length > self.capacity * self.load_factor:
-            results = [result for result in self.hash_table if result]
+            results = [
+                result for result in self.hash_table
+                if result and result is not self.DELETED]
+
             self.capacity *= 2
+
             self.hash_table = [[] for _ in range(self.capacity)]
+            self.length = 0
+
             _max_table_index = self.capacity - 1
 
             for result in results:
                 ind = result[1] % self.capacity
-                self.rehash(ind, result, _max_table_index)
+                self.add_kwargs_init(
+                    ind, result[0], result[2], _max_table_index)
 
     def __getitem__(self,
                     item: Any
                     ) -> Any:
         self.capacity = len(self.hash_table)
-        _max_index = self.capacity - 1
         ind = hash(item) % self.capacity
-        if not self.hash_table[ind]:
-            raise KeyError
 
         while True:
-            if self.hash_table[ind]:
-                if self.hash_table[ind][0] == item:
-                    return self.hash_table[ind][2]
-                elif ind == _max_index:
-                    ind = 0
-                    continue
-                ind += 1
-            else:
+            slot = self.hash_table[ind]
+            if not slot:
                 raise KeyError(f"Key {item!r} not found!")
+            if slot is not self.DELETED and slot[0] == item:
+                return slot[2]
+            ind = (ind + 1) % self.capacity
 
     def __len__(self) -> int:
         return self.length
@@ -77,35 +81,19 @@ class Dictionary:
                     ) -> None:
         self.capacity = len(self.hash_table)
         ind = hash(key) % self.capacity
-        _max_index = self.capacity - 1
-        results = []
-
-        if not self.hash_table[ind]:
-            raise KeyError(f"Key {key} not found!")
 
         while True:
-            if self.hash_table[ind]:
-                if self.hash_table[ind][0] == key:
-                    self.hash_table[ind] = []
-                    self.length -= 1
-                    break
-                elif ind == _max_index:
-                    ind = 0
-                    continue
-                ind += 1
-            else:
-                raise KeyError
+            slot = self.hash_table[ind]
 
-        for table in self.hash_table:
-            if table:
-                results.append(table)
+            if slot == []:
+                raise KeyError(f"Key {key!r} not found!")
 
-        self.hash_table = [[] for _ in range(self.capacity)]
-        _max_index = self.capacity - 1
+            if slot is not self.DELETED and slot[0] == key:
+                self.hash_table[ind] = self.DELETED
+                self.length -= 1
+                return
 
-        for result in results:
-            ind = result[1] % self.capacity
-            self.rehash(ind, result, _max_index)
+            ind = (ind + 1) % self.capacity
 
     def __iter__(self) -> Iterator[Any]:
         for table in self.hash_table:
@@ -201,15 +189,13 @@ class Dictionary:
                 if self.hash_table[ind][0] == key:
                     self.hash_table[ind] = [key, hash(key), value]
                     break
-                elif ind == _max_table_index:
-                    ind = 0
-                    continue
-                ind += 1
-                continue
+                ind = (ind + 1) % self.capacity
             else:
-                self.hash_table[ind] = [key, hash(key), value]
-                self.length += 1
-                break
+                if (self.hash_table[ind] == []
+                        or self.hash_table[ind] is self.DELETED):
+                    self.hash_table[ind] = [key, hash(key), value]
+                    self.length += 1
+                    break
 
     def rehash(self,
                ind: int,

c73ccf4fe8dbcc9d1e4af40895eea90c2f41d340
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-11-16 14:01:58 +0200

    updated according to review

--- app/main.py
+++ app/main.py
@@ -1,42 +1,45 @@
 from typing import Any, Iterator, Tuple
 
 
+class OrderNode:
+    def __init__(self, key: Any) -> None:
+        self.key = key
+        self.prev = self
+        self.next = self
+
+
 class Dictionary:
     def __init__(self) -> None:
-        """
-        capacity = initial capacity of dictionary,
-        loadFactor = occupancy rate
-        threshold = maximum occupancy
-        blocks = dict memory block
-        order = blocks order
-        count = count of active blocks
-        """
         self.capacity = 8
         self.loadFactor = 0.66
         self.threshold = int(self.capacity * self.loadFactor)
-
         self.blocks = [[] for _ in range(self.capacity)]
-        self.order = []
+
+        self.root = OrderNode(None)
         self.count = 0
 
+    def __order_append(self, key: Any) -> OrderNode:
+        new_node = OrderNode(key)
+
+        last = self.root.prev
+        root = self.root
+
+        new_node.prev = last
+        new_node.next = root
+
+        last.next = new_node
+        root.prev = new_node
+
+        return new_node
+
+    def __order_delete(self, node: OrderNode) -> None:
+        if node is not self.root:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            node.prev = None
+            node.next = None
+
     def __setitem__(self, key: Any, value: Any) -> None:
-        """
-        key_hash = searching hash of key
-        key_block = searching block for saving
-        block_status_option: ["Alive", "Dead"]
-            "Alive" -> active block
-            "Dead" -> block was removed
-        block_value: values from block
-
-        if len(self.order) >= self.threshold:
-        when filled blocks count equal to our maximum occupancy,
-        we update our capacity
-
-        for i in range(self.capacity):
-        We search key block in our blocks.
-        If hash works correct we will save our value at first iteration,
-        else until find empty or dead block
-        """
         try:
             key_hash = hash(key)
         except TypeError:
@@ -48,15 +51,15 @@ class Dictionary:
         for i in range(self.capacity):
             index = (key_hash + i) % self.capacity
             block = self.blocks[index]
-
-            if not block or block[-1] == "Dead":
-                self.blocks[index] = [key_hash, key, value, "Alive"]
-                self.order.append(key)
+            if not block or block[3] == "Dead":
+                new_node = self.__order_append(key)
+                self.blocks[index] = [key_hash, key, value, "Alive", new_node]
                 self.count += 1
                 return
 
             if block[0] == key_hash and block[1] == key:
-                self.blocks[index] = [key_hash, key, value, "Alive"]
+                node = block[4]
+                self.blocks[index] = [key_hash, key, value, "Alive", node]
                 return
 
     def __getitem__(self, key: Any) -> Any:
@@ -72,7 +75,7 @@ class Dictionary:
             if not block:
                 raise KeyError("Key not exist")
 
-            status = block[-1]
+            status = block[3]
 
             if status == "Dead":
                 continue
@@ -91,30 +94,30 @@ class Dictionary:
         self.count = 0
 
         for block in previous_blocks:
-            if not block or block[-1] == "Dead":
+            if not block or block[3] == "Dead":
                 continue
 
             key_hash = block[0]
             key = block[1]
             value = block[2]
+            node = block[4]
 
             for i in range(self.capacity):
                 index = (key_hash + i) % self.capacity
 
                 target_slot = self.blocks[index]
 
-                if not target_slot or target_slot[-1] == "Dead":
-                    self.blocks[index] = [key_hash, key, value, "Alive"]
+                if not target_slot or target_slot[3] == "Dead":
+                    self.blocks[index] = [key_hash, key, value, "Alive", node]
                     self.count += 1
                     break
 
     def __len__(self) -> int:
-        return len(self.order)
+        return self.count
 
     def __str__(self) -> str:
         parts = []
-
-        for key in self.order:
+        for key in self:
             repr_value = repr(self.__getitem__(key))
             current_key = repr(key)
             parts.append(f"{current_key}: {repr_value}")
@@ -123,7 +126,8 @@ class Dictionary:
 
     def clear(self) -> None:
         self.blocks = [[] for _ in range(self.capacity)]
-        self.order = []
+        self.root = OrderNode(None)
+        self.count = 0
 
     def __delitem__(self, key: Any) -> None:
         try:
@@ -137,12 +141,20 @@ class Dictionary:
             if not block:
                 break
 
-            if block[-1] == "Dead":
+            if block[3] == "Dead":
                 continue
 
             if block[0] == key_hash and block[1] == key:
-                self.blocks[index] = [key_hash, None, None, "Dead"]
-                self.order.remove(key)
+                node_to_delete = block[4]
+                self.__order_delete(node_to_delete)
+
+                self.blocks[index] = [
+                    key_hash,
+                    None,
+                    None,
+                    "Dead",
+                    node_to_delete
+                ]
                 self.count -= 1
                 return
 
@@ -186,11 +198,16 @@ class Dictionary:
             self.__setitem__(key, value)
 
     def __iter__(self) -> Iterator[Any]:
-        return iter(self.order)
+        current = self.root.next
+        while current is not self.root:
+            yield current.key
+            current = current.next
 
     def items(self) -> Iterator[Tuple[Any, Any]]:
-        for key in self.order:
-            yield key, self.__getitem__(key)
+        current = self.root.next
+        while current is not self.root:
+            yield current.key, self.__getitem__(current.key)
+            current = current.next
 
     def keys(self) -> list:
-        return list(self.order)
+        return list(self.__iter__())

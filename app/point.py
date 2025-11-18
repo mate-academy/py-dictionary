4350f8b19605480bd07757c9975bc0f5e672a775
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2025-11-18 10:16:31 +0200

    Flake8

--- app/point.py
+++ app/point.py
@@ -1,5 +1,5 @@
 from typing import Any
-from app.main import Dictionary
+
 
 class Point:
     def __init__(self, x: float, y: float) -> None:
@@ -26,5 +26,3 @@ class Point:
     @property
     def y(self) -> float:
         return self._y
-
-

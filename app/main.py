b34b556388904e3d2c2142532666e50f528f5763
artem <artemasarkisan598@gmail.com>
2025-12-30 20:53:28 +0200

    py-Dictionary

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from typing import Any
 
 
 class Dictionary:
+
+    LOAD_FACTOR = 0.75
+
     def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
         self.table = [[] for _ in range(capacity)]
@@ -20,6 +23,9 @@ class Dictionary:
         bucket.append((key, value, key_hash))
         self.size += 1
 
+        if self.size / self.capacity > self.LOAD_FACTOR:
+            self._resize()
+
     def __getitem__(self, key: Any) -> Any:
         key_hash = hash(key)
         index = key_hash % self.capacity
@@ -33,3 +39,13 @@ class Dictionary:
 
     def __len__(self) -> int:
         return self.size
+
+    def _resize(self) -> None:
+        old_table = self.table
+        self.capacity *= 2
+        self.table = [[] for _ in range(self.capacity)]
+        self.size = 0
+
+        for bucket in old_table:
+            for k, v, h in bucket:
+                self[k] = v

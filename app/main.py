d0dcde0ac73aed6786d41e6b5850e19d66a91cf1
artem <artemasarkisan598@gmail.com>
2025-12-30 20:40:04 +0200

    py-Dictionary

--- app/main.py
+++ app/main.py
@@ -2,33 +2,34 @@ from typing import Any
 
 
 class Dictionary:
-
-    def __init__(self, capacity=8) -> None:
+    def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
         self.table = [[] for _ in range(capacity)]
         self.size = 0
 
-    def __setitem__(self, key: Any, value: Any) -> Any:
-        index = hash(key) % self.capacity
-        buck = self.table[index]
+    def __setitem__(self, key: Any, value: Any) -> None:
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        bucket = self.table[index]
 
-        for i, (k, v) in enumerate(buck):
-            if k == key:
-                buck[i] = (key, value)
+        for i, (k, v, h) in enumerate(bucket):
+            if h == key_hash and k == key:
+                bucket[i] = (key, value, key_hash)
                 return
 
-        buck.append((key, value))
+        bucket.append((key, value, key_hash))
         self.size += 1
 
-    def __getitem__(self, item: Any) -> Any:
-        index = hash(item) % self.capacity
-        back = self.table[index]
+    def __getitem__(self, key: Any) -> Any:
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        bucket = self.table[index]
 
-        for k, v in back:
-            if k == item:
+        for k, v, h in bucket:
+            if h == key_hash and k == key:
                 return v
 
-        raise KeyError(item)
+        raise KeyError(key)
 
     def __len__(self) -> int:
         return self.size

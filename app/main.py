345f9f05097d0dee3712103ab4cbd4ae276ae9ae
titoiriepal <titoiriepal@hotmail.com>
2026-01-30 14:21:40 +0100

    getitem and len methods added to class Dictionary

--- app/main.py
+++ app/main.py
@@ -28,6 +28,30 @@ class Dictionary:
         if self.capacity * self.load_factor < self.count_items:
             self.__resize()
 
+    def __getitem__(self, key: any) -> any:
+        start_index = hash(key) % self.capacity
+        slot = self.__search_slot(key, start_index)
+        if slot is None:
+            raise KeyError(key)
+        return self.table[slot][2]
+
+    def __len__(self) -> int:
+        return self.count_items
+
+    def __search_slot(
+        self: Self,
+        key: any,
+        start_index: int,
+    ) -> int | None:
+        start_index %= self.capacity
+        for step in range(self.capacity):
+            index = (start_index + step) % self.capacity
+            if self.table[index] is None:
+                return None
+            if self.table[index][1] == key:
+                return index
+        return None
+
     def __find_slot(
         self: Self,
         key: any,
@@ -53,12 +77,15 @@ class Dictionary:
 
     def __resize(self: Self) -> None:
         old_table = self.table
-        self.table = [None for _ in range(self.capacity * 2)]
+        self.capacity *= 2
+        self.count_items = 0
+        self.table = [None for _ in range(self.capacity)]
         for item in old_table:
-            hash_key = item[0]
-            start_index = hash_key % self.capacity
-            slot = self.__find_slot(item[1], start_index)
-            if slot is None:
-                raise RuntimeError("No free slot found")
+            if item is not None:
+                hash_key = item[0]
+                start_index = hash(item[1])
+                slot = self.__find_slot(item[1], start_index)
+                if slot is None:
+                    raise RuntimeError("No free slot found")
 
-            self.__write_slot(slot, hash_key, item[1], item[2])
+                self.__write_slot(slot, hash_key, item[1], item[2])

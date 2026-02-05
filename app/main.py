2df2a439f517a535add2c6fd045a2cc673d26fc0
Marian Hrytsan <gricanmaran@gmail.com>
2026-02-05 16:05:33 +0200

    Not corect all problem

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 class Dictionary:
-    capacity = 8
-    threshold = capacity * (2 / 3)
-    size_now = 0
-    hash_table = [[]] * capacity
+    def __init__(self):
+        self.capacity = 8
+        self.threshold = self.capacity * (2 / 3)
+        self.size_now = 0
+        self.hash_table = [None] * self.capacity
 
     def __setitem__(self, key, value):
         if self.size_now <= self.threshold:
@@ -15,7 +16,8 @@ class Dictionary:
                 if self.hash_table[tmp_index][0] == key:
                     self.hash_table[tmp_index][2] = value
                 else:
-                    for index in range(self.capacity):
+                    for i in range(tmp_index):
+                        index = (tmp_index + i) % self.capacity
                         if not self.hash_table[index]:
                             self.hash_table[index] = [key, tmp_hash, value]
                             self.size_now += 1
@@ -26,7 +28,7 @@ class Dictionary:
             self.size_now = 0
             self.hash_table.clear()
             self.threshold = self.capacity * (2 / 3)
-            self.hash_table = [[]] * self.capacity
+            self.hash_table = [None] * self.capacity
             for element in tmp_hash_table:
                 if element:
                     self.__setitem__(element[0], element[2])
@@ -38,11 +40,12 @@ class Dictionary:
             if self.hash_table[tmp_index][0] == key:
                 return self.hash_table[tmp_index][2]
         else:
-            for index in range(self.capacity):
+            for i in range(tmp_index):
+                index = (tmp_index + i) % self.capacity
                 if self.hash_table[index]:
                     if self.hash_table[index][0] == key:
                         return self.hash_table[index][2]
-        raise KeyError
+        raise KeyError(f"Key '{key}' not found")
 
     def __len__(self):
         return self.size_now

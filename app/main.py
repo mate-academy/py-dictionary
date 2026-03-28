c8990401f3396ff233b16a2b1de0f91a3965fb66
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-03-28 14:55:19 +0000

    1st save

--- app/main.py
+++ app/main.py
@@ -1,2 +1,34 @@
 class Dictionary:
-    pass
+    def __init__(self):
+        self.capacity = 8
+        self.dictionary = [None] * self.capacity
+
+
+
+    def __setitem__(self, key, value):
+        index = hash(key) % self.capacity
+        if self.dictionary[index] is None:
+            self.dictionary[index] = []
+        for i, (k, v) in enumerate(self.dictionary[index]):
+            if k == key:
+                self.dictionary[index][i] = (key, value)
+                return
+        self.dictionary[index].append((key, value))
+
+
+    def __getitem__(self, key):
+        index = hash(key) % self.capacity
+        if self.dictionary[index] is None:
+            raise KeyError
+        for i, (k, v) in enumerate(self.dictionary[index]):
+            if k == key:
+                return v
+        raise KeyError
+
+
+    def __len__(self):
+        count = 0
+        for i in self.dictionary:
+            if i is not None:
+                count += len(i)
+        return count

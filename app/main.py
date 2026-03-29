a24293cd05ec22d1bef57d8db33af6b41f0d10d7
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-03-29 15:56:27 +0100

    2nd save

--- app/main.py
+++ app/main.py
@@ -2,33 +2,43 @@ class Dictionary:
     def __init__(self):
         self.capacity = 8
         self.dictionary = [None] * self.capacity
-
-
+        self.size = 0
 
     def __setitem__(self, key, value):
-        index = hash(key) % self.capacity
+        load_factor = self.size / self.capacity
+        if load_factor > (2 / 3):
+            self._resize()
+
+        h = hash(key)
+        index = h % self.capacity
         if self.dictionary[index] is None:
             self.dictionary[index] = []
-        for i, (k, v) in enumerate(self.dictionary[index]):
+        for i, (k, stored_hash, v) in enumerate(self.dictionary[index]):
             if k == key:
-                self.dictionary[index][i] = (key, value)
+                self.dictionary[index][i] = (key, h, value)
                 return
-        self.dictionary[index].append((key, value))
-
+        self.dictionary[index].append((key, h, value))
+        self.size += 1
 
     def __getitem__(self, key):
-        index = hash(key) % self.capacity
+        h = hash(key)
+        index = h % self.capacity
         if self.dictionary[index] is None:
-            raise KeyError
-        for i, (k, v) in enumerate(self.dictionary[index]):
-            if k == key:
+            raise KeyError(f"The {self.dictionary} is None")
+        for i, (k, stored_hash, v) in enumerate(self.dictionary[index]):
+            if k == key and stored_hash == h:
                 return v
-        raise KeyError
+        raise KeyError(f"Key {key} is not in the dictionary")
 
+    def _resize(self):
+        old = self.dictionary
+        self.capacity *= 2
+        self.dictionary = [None] * self.capacity
+        self.size = 0
+        for slot in old:
+            if slot:
+                for key, h, value in slot:
+                    self[key] = value
 
     def __len__(self):
-        count = 0
-        for i in self.dictionary:
-            if i is not None:
-                count += len(i)
-        return count
+        return self.size

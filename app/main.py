54aabd39893b497d1edb34ebdb76e0ff69aad774
Neyrony <sserebriy@gmail.com>
2026-04-09 18:11:04 +0300

    Implemented mock and not optimized solution for __setitem__ and related func with test

--- app/main.py
+++ app/main.py
@@ -4,36 +4,33 @@ from copy import deepcopy
 from typing import Any
 
 
+@dataclass
+class Element:
+    key: Hashable
+    hash: int
+    value: Any
+
+
 class Dictionary:
     LOAD_FACTOR = 2 / 3
 
     def __init__(self):
         self.__capacity = 8
-        self.__slots: list[None | Element]= [None] * self.__capacity
+        self.__slots: list[None | Element] = [None] * self.__capacity
         self.__new_slots = []
-        self.__count_not_none_elem = 0
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        self.set_place(key, value, self.__slots)
+        if self.count_not_none_elem() + 1 > int(self.__capacity * self.LOAD_FACTOR):
+            self.resize_table()
+            self.__slots = deepcopy(self.__new_slots)
+            self.__new_slots.clear()
 
+        self.set_place(key, value, self.__slots)
 
     def set_place(self, key: Hashable, value: Any, list_of_slots: list) -> None:
         place_dependent_on_hash = hash(key) % self.__capacity
         while True:
-            if not list_of_slots[place_dependent_on_hash]:
-                self.__count_not_none_elem += 1
-                if self.count_not_none_elem() > int(self.__capacity * self.LOAD_FACTOR):
-                    self.resize_table()
-                    self.__slots = deepcopy(self.__new_slots)
-                    self.__new_slots.clear()
-
-                list_of_slots[place_dependent_on_hash] = Element(
-                    key=key,
-                    value=value,
-                    hash=hash(key)
-                )
-                break
-            elif list_of_slots[place_dependent_on_hash].key == key:
+            if not list_of_slots[place_dependent_on_hash] or list_of_slots[place_dependent_on_hash].key == key:
                 list_of_slots[place_dependent_on_hash] = Element(
                     key=key,
                     value=value,
@@ -51,14 +48,15 @@ class Dictionary:
             if element:
                 self.set_place(element.key, element.value, self.__new_slots)
 
-
     def count_not_none_elem(self) -> int:
         return sum(1 for elem in self.__slots if elem)
 
 
-@dataclass
-class Element:
-    key: Hashable
-    hash: int
-    value: Any
-
+d = Dictionary()
+d[1] = "123"
+d[2] = "456"
+d[2] = "789"
+d[12] = "qwe"
+d[4] = "rty"
+d[6] = "zxc"
+d[9] = "asd"

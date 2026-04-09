50ed451ba6785081935e75bb0f693a59b3bdf623
Neyrony <sserebriy@gmail.com>
2026-04-09 19:13:33 +0300

    Dict set method works

--- app/main.py
+++ app/main.py
@@ -14,13 +14,19 @@ class Element:
 class Dictionary:
     LOAD_FACTOR = 2 / 3
 
-    def __init__(self):
+    def __init__(self, **kwargs: Any) -> None:
         self.__capacity = 8
         self.__slots: list[None | Element] = [None] * self.__capacity
         self.__new_slots = []
+        self.__count_not_none_elem = 0
+
+        for key, value in kwargs.items():
+            self[key] = value
+
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        if self.count_not_none_elem() + 1 > int(self.__capacity * self.LOAD_FACTOR):
+        key_exist = any(key == elem.key for elem in self.__slots if elem)
+        if self.__count_not_none_elem + 1 > int(self.__capacity * self.LOAD_FACTOR) and not key_exist:
             self.resize_table()
             self.__slots = deepcopy(self.__new_slots)
             self.__new_slots.clear()
@@ -31,6 +37,7 @@ class Dictionary:
         place_dependent_on_hash = hash(key) % self.__capacity
         while True:
             if not list_of_slots[place_dependent_on_hash] or list_of_slots[place_dependent_on_hash].key == key:
+                self.__count_not_none_elem += 1 if list_of_slots is self.__slots  and not list_of_slots[place_dependent_on_hash] else 0
                 list_of_slots[place_dependent_on_hash] = Element(
                     key=key,
                     value=value,
@@ -51,12 +58,17 @@ class Dictionary:
     def count_not_none_elem(self) -> int:
         return sum(1 for elem in self.__slots if elem)
 
+    def __getitem__(self, key: Hashable) -> Any:
+        pass
 
-d = Dictionary()
+
+d = Dictionary(solo=1, duo=2)
 d[1] = "123"
 d[2] = "456"
 d[2] = "789"
 d[12] = "qwe"
 d[4] = "rty"
 d[6] = "zxc"
+d[1] = "jkl"
 d[9] = "asd"
+print

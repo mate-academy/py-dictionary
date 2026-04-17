14c1d721cb3ad9e291a690103ad5b1def2e9f070
Artem <artemradckevich@gmail.com>
2026-04-17 15:58:37 +0300

    Implemented method resize in class Dictionary

--- app/main.py
+++ app/main.py
@@ -17,21 +17,19 @@ class Dictionary:
 
     def __setitem__(self, key: Any, value: Any) -> None:
         hash_key = hash(key)
+        if (self.size + 1) / self.capacity > (2 / 3):
+            self.resize()
         index = hash_key % self.capacity
         bucket = self.buckets[index]
         if bucket is None:
-            bucket_buckets = []
-            node = Node(key, value, hash_key)
-            bucket_buckets.append(node)
-            self.buckets[index] = bucket_buckets
+            self.buckets[index] = [Node(key, value, hash_key)]
             self.size += 1
             return
         for node in bucket:
             if node.key == key:
                 node.value = value
                 return
-        node = Node(key, value, hash_key)
-        bucket.append(node)
+        bucket.append(Node(key, value, hash_key))
         self.size += 1
 
     def __getitem__(self, item: Any) -> Any:
@@ -47,3 +45,17 @@ class Dictionary:
 
     def __len__(self) -> int:
         return self.size
+
+    def resize(self) -> None:
+        old_buckets = self.buckets
+        self.capacity *= 2
+        self.buckets = [None] * self.capacity
+        self.size = 0
+        for bucket in old_buckets:
+            if bucket is not None:
+                for node in bucket:
+                    new_index = node.hash_key % self.capacity
+                    if self.buckets[new_index] is None:
+                        self.buckets[new_index] = []
+                    self.buckets[new_index].append(node)
+                    self.size += 1

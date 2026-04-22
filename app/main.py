bcc8e975bec871eb7a910c7d4e9803074ea5ed03
zubak-vasil <zvasil7@gmail.com>
2026-04-22 23:24:48 +0300

    implementing-solution-for-py-dictionary-fixed

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ class Dictionary:
     def __init__(self) -> None:
         self.capacity: int = 8
         self.length: int = 0
-        self.table: list[Optional[tuple]] = [None] * self.capacity
+
+        self.table: list[Optional[tuple[Any, int, Any]]] = (
+            [None] * self.capacity
+        )
 
     def __setitem__(self, key: Any, value: Any) -> None:
         if self.length / self.capacity > 0.66:
@@ -17,22 +20,28 @@ class Dictionary:
         while self.table[index] is not None:
             current_item = self.table[index]
 
-            if current_item is not None and current_item[0] == key:
-                self.table[index] = (key, value)
+            if (current_item is not None
+                    and current_item[1] == key_hash
+                    and current_item[0] == key):
+                self.table[index] = (key, key_hash, value)
                 return
             index = (index + 1) % self.capacity
 
-        self.table[index] = (key, value)
+        self.table[index] = (key, key_hash, value)
         self.length += 1
 
     def __getitem__(self, key: Any) -> Any:
-        index = hash(key) % self.capacity
+        key_hash = hash(key)
+        index = key_hash % self.capacity
         initial_index = index
 
         while self.table[index] is not None:
             current_item = self.table[index]
-            if current_item is not None and current_item[0] == key:
-                return current_item[1]
+
+            if (current_item is not None
+                    and current_item[1] == key_hash
+                    and current_item[0] == key):
+                return current_item[2]
 
             index = (index + 1) % self.capacity
             if index == initial_index:
@@ -51,4 +60,13 @@ class Dictionary:
 
         for item in old_table:
             if item is not None:
-                self.__setitem__(item[0], item[1])
+                stor_key = item[0]
+                stor_hash = item[1]
+                stor_value = item[2]
+
+                index = stor_hash % self.capacity
+                while self.table[index] is not None:
+                    index = (index + 1) % self.capacity
+
+                self.table[index] = (stor_key, stor_hash, stor_value)
+                self.length += 1

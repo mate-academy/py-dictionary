7ba750958d28ec9046156b150161a67030f2eb2d
Sergey <sobka2702@gmail.com>
2026-04-23 15:36:17 +0300

    added fix and annotation

--- app/main.py
+++ app/main.py
@@ -1,113 +1,118 @@
-class Pair:
-    def __init__(self, key, hash_value, value):
-        self.key = key
-        self.hash = hash_value
-        self.value = value
+from dataclasses import dataclass
+from typing import Hashable, Any, Iterator
 
 
-class Dictionary:
-    def __init__(self, initial_capacity=8, load_factor=0.75):
-        self._capacity = initial_capacity
-        self._load_factor = load_factor
+@dataclass(slots=True)
+class Node:
+    key: Hashable
+    value: Any
+    hash_value: int
+
+
+class CustomDict:
+    INITIAL_CAPACITY = 8
+    LOAD_FACTOR = 2 / 3
+    CAPACITY_MULTIPLIER = 2
+
+    def __init__(self) -> None:
+        self._table: list[Node | None] = [None] * self.INITIAL_CAPACITY
         self._size = 0
-        self._buckets = [[] for _ in range(self._capacity)]
+        self._capacity = self.INITIAL_CAPACITY
 
-    def __len__(self):
+    @property
+    def _threshold(self) -> Any:
+        return self._capacity * self.LOAD_FACTOR
+
+    def _linear_probing(self, index: int) -> int:
+        return (index + 1) % self._capacity
+
+    def _calculate_index(self, key: Hashable) -> int:
+        hash_value = hash(key)
+        index = hash_value % self._capacity
+        while (
+                (node := self._table[index]) is not None
+                and node.hash_value != hash_value
+                and node.key != key
+        ):
+            index = self._linear_probing(index)
+        return index
+
+    def _resize(self) -> None:
+        old_table = self._table
+        self._capacity *= self.CAPACITY_MULTIPLIER
+        self._table = [None] * self._capacity
+        self._size = 0
+        for node in old_table:
+            if node is not None:
+                self[node.key] = node.value
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        index = self._calculate_index(key)
+        if (node := self._table[index]) is None:
+            if self._size + 1 > self._threshold:
+                self._resize()
+                index = self._calculate_index(key)
+            self._table[index] = Node(key, value, hash(key))
+            self._size += 1
+        else:
+            node.value = value
+
+    def __getitem__(self, key: Hashable) -> Any:
+        index = self._calculate_index(key)
+        if (node := self._table[index]) is None:
+            raise KeyError
+        return node.value
+
+    def __len__(self) -> int:
         return self._size
 
-    def _check_index(self, hash_value):
-        return hash_value % self._capacity
+    def __iter__(self) -> Iterator[Hashable]:
+        for node in self._table:
+            if node is not None:
+                yield node.key
 
-    def __setitem__(self, key, value):
-        hash_value = hash(key)
-        index = self._check_index(hash_value)
-        bucket = self._buckets[index]
+    def get(self, key: Hashable, default: Any = None) -> Any:
+        index = self._calculate_index(key)
+        node = self._table[index]
 
-        for pair in bucket:
-            if pair.hash == hash_value and pair.key == key:
-                pair.value = value
-                return
-
-        bucket.append(Pair(key, hash_value, value))
-        self._size += 1
-
-        if self._size / self._capacity > self._load_factor:
-            self._resize()
-
-    def __getitem__(self, key):
-        hash_value = hash(key)
-        index = self._check_index(hash_value)
-        bucket = self._buckets[index]
-
-        for pair in bucket:
-            if pair.hash == hash_value and pair.key == key:
-                return pair.value
-
-        raise KeyError(key)
-
-    def __delitem__(self, key):
-        hash_value = hash(key)
-        index = self._check_index(hash_value)
-        bucket = self._buckets[index]
-
-        for i, pair in enumerate(bucket):
-            if pair.hash == hash_value and pair.key == key:
-                del bucket[i]
-                self._size -= 1
-                return
-
-        raise KeyError(key)
-
-    def get(self, key, default=None):
-        hash_value = hash(key)
-        index = self._check_index(hash_value)
-        bucket = self._buckets[index]
-
-        for pair in bucket:
-            if pair.hash == hash_value and pair.key == key:
-                return pair.value
-
-        return default
-
-    def pop(self, key, default=None):
-        hash_value = hash(key)
-        index = self._check_index(hash_value)
-        bucket = self._buckets[index]
-
-        for i, pair in enumerate(bucket):
-            if pair.hash == hash_value and pair.key == key:
-                value = pair.value
-                del bucket[i]
-                self._size -= 1
-                return value
-
-        if default is not None:
+        if node is None:
             return default
 
-        raise KeyError(key)
+        return node.value
 
-    def clear(self):
-        self._buckets = [[] for _ in range(self._capacity)]
+    def clear(self) -> None:
+        self._table = [None] * self.INITIAL_CAPACITY
         self._size = 0
+        self._capacity = self.INITIAL_CAPACITY
 
-    def update(self, other):
+    def update(self, other: Any) -> None:
         for key, value in other:
             self[key] = value
 
-    def __iter__(self):
-        for bucket in self._buckets:
-            for pair in bucket:
-                yield pair.key, pair.value
+    def pop(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            value = self[key]
+            del self[key]
+            return value
+        except KeyError:
+            if default is not None:
+                return default
+            raise KeyError(key)
 
-    def _resize(self):
-        old_buckets = self._buckets
-        self._capacity *= 2
-        self._buckets = [[] for _ in range(self._capacity)]
-        old_size = self._size
-        self._size = 0
+    def __delitem__(self, key: Hashable) -> None:
+        index = self._calculate_index(key)
 
-        for bucket in old_buckets:
-            for pair in bucket:
-                self[pair.key] = pair.value
+        if self._table[index] is None:
+            raise KeyError(key)
 
-        self._size = old_size
+        self._table[index] = None
+        self._size -= 1
+
+        next_index = self._linear_probing(index)
+
+        while self._table[next_index] is not None:
+            node_to_rehash = self._table[next_index]
+            self._table[next_index] = None
+            self._size -= 1
+            self[node_to_rehash.key] = node_to_rehash.value
+            next_index = self._linear_probing(next_index)

575844b83f48ef9cfbe76aa27fac6e9d99fae85b
David <meeddry@gmail.com>
2026-04-23 09:41:35 +0300

    1.2_dict_ver

--- app/main.py
+++ app/main.py
@@ -1,31 +1,37 @@
-from typing import Any
+from typing import Any, Optional
 
 
 class Dictionary:
     def __init__(self) -> None:
-        self.table = [None] * 8
-        self.size = 0
+        self.table: list[Optional[list[tuple[Any, int, Any]]]] = [None] * 8
+        self.size: int = 0
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        index = hash(key) % len(self.table)
+        key_hash = hash(key)
+        index = key_hash % len(self.table)
 
         if self.table[index] is None:
-            self.table[index] = [(key, value)]
+            self.table[index] = [(key, key_hash, value)]
             self.size += 1
         else:
-            for i, (k, v) in enumerate(self.table[index]):
-                if k == key:
-                    self.table[index][i] = (key, value)
+            bucket = self.table[index]
+
+            for i, (k, h, v) in enumerate(bucket):
+                if h == key_hash and k == key:
+                    bucket[i] = (key, key_hash, value)
                     return
 
-            self.table[index].append((key, value))
+            bucket.append((key, key_hash, value))
             self.size += 1
 
     def __getitem__(self, key: Any) -> Any:
-        index = hash(key) % len(self.table)
-        if self.table[index] is not None:
-            for k, v in self.table[index]:
-                if k == key:
+        key_hash = hash(key)
+        index = key_hash % len(self.table)
+
+        bucket = self.table[index]
+        if bucket is not None:
+            for k, h, v in bucket:
+                if h == key_hash and k == key:
                     return v
 
         raise KeyError(key)

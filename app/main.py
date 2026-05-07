2ae24bd844470e5c5389589a71413ec5e3e139d6
Pawel_Florczyk <pawelf775@gmail.com>
2026-05-07 15:22:04 +0200

    Solve

--- app/main.py
+++ app/main.py
@@ -1,2 +1,52 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.load_factor = 2 / 3
+        self.capacity = 8
+        self.hash_list: list = [None] * self.capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        index = hash(key) % self.capacity
+        while self.hash_list[index] is not None:
+            if self.hash_list[index][0] == key:
+                self.hash_list[index] = [key, value]
+                return
+
+            index = (index + 1) % self.capacity
+
+        self.hash_list[index] = [key, value]
+        self.length += 1
+
+        if self.length > self.load_factor * self.capacity:
+            old_hash_list = self.hash_list
+            self.capacity *= 2
+            self.hash_list: list = [None] * self.capacity
+            self.length = 0
+
+            for obj in old_hash_list:
+                if obj is not None:
+                    self.__setitem__(obj[0], obj[1])
+
+    def __delitem__(self, key: Any) -> None:
+        index = hash(key) % self.capacity
+        while self.hash_list[index] is not None:
+            if self.hash_list[index][0] == key:
+                self.hash_list[index][1] = None
+        self.length -= 1
+
+    def __getitem__(self, key: Any) -> Any:
+        index = hash(key) % self.capacity
+        while self.hash_list[index] is not None:
+            old_key, value = self.hash_list[index]
+            if old_key == key:
+                return value
+
+            index = (index + 1) % self.capacity
+
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.length

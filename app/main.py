51abd003bdef6b17b4bf30b4289de6a0651f015e
Yurii <yura.gvozd.2002@gmail.com>
2026-05-31 20:45:16 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,57 +1,56 @@
+from typing import Any
+
+
 class Dictionary:
     def __init__(self) -> None:
-        self.table = [None] * 5
-        self.size = 0
+        self.table: list[Any] = [None] * 5
+        self.size: int = 0
 
     def resize(self) -> None:
         old_table = self.table
 
-        # збільшуємо таблицю вдвічі
         self.table = [None] * (len(old_table) * 2)
 
-        # обнуляємо size, бо будемо додавати елементи заново
         old_size = self.size
         self.size = 0
 
-        # переносимо всі елементи
         for bucket in old_table:
             if bucket is not None:
-                for key, value in bucket:
+                for key, key_hash, value in bucket:
                     self[key] = value
 
         self.size = old_size
 
-    def __setitem__(self, key: int, value: int) -> None:
-        # якщо таблиця заповнена більше ніж на 70%
+    def __setitem__(self, key: Any, value: Any) -> None:
         if self.size / len(self.table) > 0.7:
             self.resize()
 
-        index = hash(key) % len(self.table)
+        key_hash = hash(key)
+        index = key_hash % len(self.table)
 
         if self.table[index] is None:
             self.table[index] = []
 
-        # оновлення існуючого ключа
         for pair in self.table[index]:
             if pair[0] == key:
-                pair[1] = value
+                pair[2] = value
                 return
 
-        # додавання нового ключа
-        self.table[index].append([key, value])
+        self.table[index].append([key, key_hash, value])
         self.size += 1
 
-    def __getitem__(self, key: int) -> None:
-        index = hash(key) % len(self.table)
+    def __getitem__(self, key: Any) -> Any:
+        key_hash = hash(key)
+        index = key_hash % len(self.table)
 
         if self.table[index] is None:
-            raise KeyError(key)
+            raise KeyError(f"Key '{key}' not found in dictionary")
 
         for pair in self.table[index]:
             if pair[0] == key:
-                return pair[1]
+                return pair[2]
 
-        raise KeyError(key)
+        raise KeyError(f"Key '{key}' not found in dictionary")
 
-    def __len__(self) -> None:
+    def __len__(self) -> int:
         return self.size

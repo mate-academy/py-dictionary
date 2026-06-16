63a32405cadd09cad268acb09d0d8965e6f1babb
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2026-06-17 01:23:42 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ from typing import Any
 class Dictionary:
     def __init__(self) -> None:
         self.capacity = 8
-        self.hash_table = [[] for _ in range(self.capacity)]
+        self.length = 0
+        self.hash_table = [None for _ in range(self.capacity)]
 
     def get_index(self, key: Any) -> int:
         return hash(key) % self.capacity
 
     def find_slot(self, key: Any, index: int) -> tuple[bool, int]:
-        while self.hash_table[index]:
+        while self.hash_table[index] is not None:
             if self.hash_table[index][0] == key:
                 return True, index
             index = (index + 1) % self.capacity
@@ -18,13 +19,14 @@ class Dictionary:
 
     def resize_table(self) -> None:
         self.capacity *= 2
-        old_nodes = [node for node in self.hash_table if node]
-        self.hash_table = [[] for _ in range(self.capacity)]
-        for node in old_nodes:
-            self[node[0]] = node[2]
+        old_nodes = [node for node in self.hash_table if node is not None]
+        self.hash_table = [None for _ in range(self.capacity)]
+        self.length = 0
+        for key, _, value in old_nodes:
+            self[key] = value
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        if len(self) == int(self.capacity * 2 / 3):
+        if self.length == int(self.capacity * 2 / 3):
             self.resize_table()
 
         index = self.get_index(key)
@@ -33,7 +35,8 @@ class Dictionary:
         if found:
             self.hash_table[slot_index][2] = value
         else:
-            self.hash_table[slot_index].extend([key, index, value])
+            self.hash_table[slot_index] = [key, hash(key), value]
+            self.length += 1
 
     def __getitem__(self, key: Any) -> Any:
         index = self.get_index(key)
@@ -45,4 +48,4 @@ class Dictionary:
             raise KeyError("Such key doesn't exist.")
 
     def __len__(self) -> int:
-        return len([node for node in self.hash_table if node])
+        return self.length

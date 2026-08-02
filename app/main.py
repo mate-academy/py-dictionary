5112319ff475d512b8e2a9170a15c7464b857577
Anna Kadlecova <adorosenko440@gmail.com>
2026-08-02 19:42:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Dictionary:
     def __init__(self) -> None:
         self.length = 0
@@ -6,7 +9,7 @@ class Dictionary:
     def __len__(self) -> int:
         return self.length
 
-    def __setitem__(self, key, value) -> None:
+    def __setitem__(self, key: Any, value: Any) -> None:
         hash_key = hash(key)
         index_key = hash_key % len(self.hash_table)
         while self.hash_table[index_key] is not None:
@@ -17,10 +20,25 @@ class Dictionary:
                 index_key = (index_key + 1) % len(self.hash_table)
         self.hash_table[index_key] = (key, hash_key, value)
         self.length += 1
-        if
+        if self.length / len(self.hash_table) > 2 / 3:
+            self.resize()
 
+    def resize(self) -> None:
+        new_hash_table = [None] * (len(self.hash_table) * 2)
+        for cell in self.hash_table:
+            if cell is not None:
+                new_index = cell[1] % len(new_hash_table)
+                while new_hash_table[new_index] is not None:
+                    new_index = (new_index + 1) % len(new_hash_table)
+                new_hash_table[new_index] = cell
+        self.hash_table = new_hash_table
 
-
-    def __getitem__(self, key):
-
-    
+    def __getitem__(self, key: Any) -> Any:
+        hash_key = hash(key)
+        index_key = hash_key % len(self.hash_table)
+        while self.hash_table[index_key] is not None:
+            if self.hash_table[index_key][0] != key:
+                index_key = (index_key + 1) % len(self.hash_table)
+            else:
+                return self.hash_table[index_key][2]
+        raise KeyError

b0685af77074b24544252b4875911bce94211474
mxrpheusd <tommyneniger@gmail.com>
2026-08-10 14:25:54 +0300

    final solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class Dictionary:
     def __init__(self) -> None:
         self.capacity = 8
         self.load_factor = 2 / 3
-        self.size = 0
+        self.length = 0
         self.slots = [None] * self.capacity
 
     def __setitem__(self, key: Any, value: Any) -> None:
@@ -20,16 +20,16 @@ class Dictionary:
             index = (index + 1) % self.capacity
 
         self.slots[index] = Node(key, value, key_hash)
-        self.size += 1
+        self.length += 1
 
-        if self.size >= self.capacity * self.load_factor:
+        if self.length >= self.capacity * self.load_factor:
             self.resize()
 
     def resize(self) -> None:
         old_slots = self.slots
         self.capacity = self.capacity * 2
         self.slots = [None] * self.capacity
-        self.size = 0
+        self.length = 0
         for slot in old_slots:
             if slot is not None:
                 self.__setitem__(slot.key, slot.value)
@@ -43,10 +43,10 @@ class Dictionary:
                 return self.slots[index].value
             index = (index + 1) % self.capacity
 
-        raise KeyError(key)
+        raise KeyError(f"Key '{key}' not found")
 
     def __len__(self) -> int:
-        return self.size
+        return self.length
 
 
 @dataclasses.dataclass

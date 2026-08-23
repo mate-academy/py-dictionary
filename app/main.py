23a9177458f72698623c442b36083752b86cf314
Alex <alex@example.com>
2026-08-23 17:38:13 +0300

    fiinish

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ class Dictionary:
     def __init__(self):
         self._capacity = 8  # початковий розмір
         self._size = 0
-        self._table = [None] * self._capacity
+        self._table = [[] for _ in range(self._capacity)]
 
     def _hash(self, key):
         return hash(key) % self._capacity
@@ -12,30 +12,18 @@ class Dictionary:
 
     def __setitem__(self, key, value):
         index = self._hash(key)
-         # Збережи ключ і значення в комірку
-        
-        if self._table[index] is None:
-            self._size += 1
-        
-        self._table[index] = (key, value, hash(key))
-          
+        # Шукаємо ключ у списку
+        for i, (stored_key, _, stored_hash) in enumerate(self._table[index]):
+            if stored_hash == hash(key) and stored_key == key:
+                self._table[index][i] = (key, value, hash(key))
+                return
+        # Ключа немає — додаємо
+        self._table[index].append((key, value, hash(key)))
+        self._size += 1
+
     def __getitem__(self, key):
         index = self._hash(key)
-        # 1. Перевір чи комірка порожня
-        if self._table[index] is None:
-            raise KeyError(key)
-            # 2. Розпакуй і порівняй ключі
-        stored_key, stored_value, stored_hash = self._table[index]
-
-        if stored_hash == hash(key) and stored_key == key:
-            return stored_value
-        # 3. Ключ не знайдено
+        for stored_key, stored_value, stored_hash in self._table[index]:
+            if stored_hash == hash(key) and stored_key == key:
+                return stored_value
         raise KeyError(key)
-
-
-
-
-d = Dictionary()
-d["apple"] = "яблуко"
-d["apple"] = "нове яблуко"  # оновлюємо
-print(len(d))  # що виведе?

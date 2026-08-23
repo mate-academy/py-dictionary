e05a2fc49493fdc5f5c3cf5e6c48afb2b7caf9d9
Alex <alex@example.com>
2026-08-23 18:33:31 +0300

    fiinish12

--- app/main.py
+++ app/main.py
@@ -1,29 +1,29 @@
 class Dictionary:
     def __init__(self) -> None:
         self._capacity = 8  # початковий розмір
-        self._size = 0
-        self._table = [[] for _ in range(self._capacity)]
+        self.length = 0
+        self.hash_table = [[] for _ in range(self._capacity)]
 
     def _hash(self, key: str) -> int:
         return hash(key) % self._capacity
 
     def __len__(self) -> int:
-        return self._size
+        return self.length
 
     def __setitem__(self, key: str, value: str) -> None:
         index = self._hash(key)
         # Шукаємо ключ у списку
-        for i, (stored_key, _, stored_hash) in enumerate(self._table[index]):
+        for i, (stored_key, _, stored_hash) in enumerate(self.hash_table[index]):
             if stored_hash == hash(key) and stored_key == key:
-                self._table[index][i] = (key, value, hash(key))
+                self.hash_table[index][i] = (key, value, hash(key))
                 return
         # Ключа немає — додаємо
-        self._table[index].append((key, value, hash(key)))
-        self._size += 1
+        self.hash_table[index].append((key, value, hash(key)))
+        self.length += 1
 
     def __getitem__(self, key: str) -> str:
         index = self._hash(key)
-        for stored_key, stored_value, stored_hash in self._table[index]:
+        for stored_key, stored_value, stored_hash in self.hash_table[index]:
             if stored_hash == hash(key) and stored_key == key:
                 return stored_value
-        raise KeyError(key)
+        raise KeyError(f'Key {key} not found')
